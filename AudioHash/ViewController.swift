fe131856e04322f9c903d6b0913f9cc24c43cc60
Sascha Schwabbauer <sascha@evolved.io>
2014-12-28 20:08:05 +0100

    Initial Commit

---
+++ AudioHash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioHash
+//
+//  Created by Sascha Schwabbauer on 28/12/14.
+//  Copyright (c) 2014 evolved.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
